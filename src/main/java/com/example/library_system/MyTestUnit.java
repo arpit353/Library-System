590abfe4418265c70e1cee995e5779b5d4fccb91
Khurana, Arpit <Arpit.Khurana@sabre.com>
2019-10-13 15:12:12 +0530

    Added Junit test

---
+++ src/main/java/com/example/library_system/MyTestUnit.java
@@ -0,0 +1,8 @@
+package com.example.library_system;
+
+public class MyTestUnit {
+
+    public String concatenate(String one, String two){
+        return one + two;
+    }
+}
--- src/test/java/com/example/library_system/LibrarySystemApplicationTests.java
+++ src/test/java/com/example/library_system/LibrarySystemApplicationTests.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.example.library_system.MyTestUnit;;
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +14,17 @@ public class LibrarySystemApplicationTests {
 
   @Test
   public void contextLoads() {
+  }
+
+  @Test
+  public void testMyTestUnit() {
+
+    MyTestUnit myUnit = new MyTestUnit();
+
+    String result = myUnit.concatenate("one", "two");
+
+    assertEquals("onetwo", result);
+
   }
 
 }
